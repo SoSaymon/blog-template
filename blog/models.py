8df4ea77e1e7ed4e70ed65221cda65f5fb7a10ee
SoSaymon <szymon.chirowski@protonmail.com>
2023-06-24 19:35:31 +0200

    Update models, add comment model

--- blog/models.py
+++ blog/models.py
@@ -80,3 +80,32 @@ class Category(models.Model):
 
     def get_absolute_url(self):
         return reverse('category-detail', args=[str(self.id)])
+
+
+class Comment(models.Model):
+    post = models.ForeignKey('Post', on_delete=models.CASCADE, related_name='comments')
+    author = models.ForeignKey('User', on_delete=models.CASCADE, related_name='comments')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    approved = models.BooleanField(default=False)
+
+    class Meta:
+        ordering = ['created_at']
+        permissions = (
+            ("can_approve", "Can approve comments"),
+            ("can_edit", "Can edit comments"),
+            ("can_delete", "Can delete comments"),
+        )
+        verbose_name = 'Comment'
+        verbose_name_plural = 'Comments'
+
+    def __str__(self):
+        return self.content
+
+    def approve(self):
+        self.approved = True
+        self.save()
+
+    def get_absolute_url(self):
+        return reverse('post-detail', args=[str(self.post.id)])
