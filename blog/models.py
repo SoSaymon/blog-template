21cd7588864e34426e44738f64662ebcf992346f
SoSaymon <szymon.chirowski@protonmail.com>
2023-06-24 15:10:44 +0200

    Add models (untested)

--- blog/models.py
+++ blog/models.py
@@ -1,3 +1,81 @@
 from django.db import models
+from django.urls import reverse
 
-# Create your models here.
+
+class Post(models.Model):
+    title = models.CharField(max_length=100, help_text='Post title')
+    content = models.TextField()
+    created_at = models.DateTimeField(auto_now_add=True)
+    updated_at = models.DateTimeField(auto_now=True)
+    author = models.ForeignKey('User', on_delete=models.CASCADE, help_text='Post author')
+    tags = models.ManyToManyField('Tag', blank=True, help_text='Post tags')
+    category = models.ForeignKey('Category', on_delete=models.SET_NULL, null=True, blank=True, default='Uncategorized',
+                                 help_text='Post category')
+
+    class Meta:
+        ordering = ['-updated_at']
+        permissions = (
+            ("can_publish", "Can publish posts"),
+            ("can_edit", "Can edit posts"),
+            ("can_delete", "Can delete posts"),
+
+        )
+
+    def __str__(self):
+        return self.title
+
+    def display_tags(self):
+        return ', '.join(tag.name for tag in self.tags.all()[:3])
+
+    def get_absolute_url(self):
+        return reverse('post-detail', args=[str(self.id)])
+
+
+class User(models.Model):
+    name = models.CharField(max_length=100, help_text='Username')
+    email = models.EmailField(max_length=100, help_text='User email')
+    created_at = models.DateTimeField(auto_now_add=True)
+
+    ROLES = (
+        ('admin', 'Admin'),
+        ('author', 'Author'),
+        ('moderator', 'Moderator'),
+        ('user', 'User'),
+    )
+
+    role = models.CharField(max_length=10, choices=ROLES, default='user', help_text='User role')
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('user-detail', args=[str(self.id)])
+
+    def get_role(self):
+        return self.role
+
+    def get_role_verbose(self):
+        return dict(User.ROLES)[self.role]
+
+    def get_role_verbose_plural(self):
+        return dict(User.ROLES)[self.role] + 's'
+
+
+class Tag(models.Model):
+    name = models.CharField(max_length=100, help_text='Tag name')
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('tag-detail', args=[str(self.id)])
+
+
+class Category(models.Model):
+    name = models.CharField(max_length=100, help_text='Category name')
+
+    def __str__(self):
+        return self.name
+
+    def get_absolute_url(self):
+        return reverse('category-detail', args=[str(self.id)])
