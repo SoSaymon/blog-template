948e5783b681a1d5a31dbafa4dccbe2899e28012
SoSaymon <szymon.chirowski@protonmail.com>
2023-06-25 16:37:58 +0200

    Add admin site

--- blog/admin.py
+++ blog/admin.py
@@ -1,3 +1,18 @@
 from django.contrib import admin
 
-# Register your models here.
+from .models import Post, Category, Tag, Comment, User
+
+admin.site.register(Post)
+admin.site.register(Category)
+admin.site.register(Tag)
+admin.site.register(Comment)
+admin.site.register(User)
+
+# admin site customization
+admin.site.site_header = 'Blog Admin'
+admin.site.site_title = 'Blog Admin'
+admin.site.index_title = 'Blog Admin'
+
+
+
+
