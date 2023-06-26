564b61a566562c3a9a2aa030a85ddc62f572ebfe
SoSaymon <szymon.chirowski@protonmail.com>
2023-06-26 08:05:24 +0200

    Add post list, detail view, test views, Update index, base_generic, urls, views

--- blog/urls.py
+++ blog/urls.py
@@ -2,5 +2,7 @@ from django.urls import path
 from . import views
 
 urlpatterns = [
-
+    path('', views.index, name='index'),
+    path('posts/', views.PostListView.as_view(), name='posts'),
+    path('post/<int:pk>/', views.PostDetailView.as_view(), name='post-detail'),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,23 @@
 from django.shortcuts import render
+from django.views import generic
 
-# Create your views here.
+from blog.models import Post
+
+
+def index(request):
+    posts = Post.objects.order_by('-created_at')[:4]
+    context = {'posts': posts}
+    return render(request, 'index.html', context=context)
+
+
+class PostListView(generic.ListView):
+    model = Post
+    paginate_by = 10
+    context_object_name = 'posts'
+    queryset = Post.objects.order_by('-created_at')
+
+
+class PostDetailView(generic.DetailView):
+    model = Post
+    context_object_name = 'post'
+
