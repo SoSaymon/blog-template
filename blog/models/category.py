72081a8d7cc2cc5c32a3b7cac4fce37464a47b04
SoSaymon <szymon.chirowski@protonmail.com>
2023-06-26 13:58:10 +0200

    Fix plural for Category class

--- blog/models/category.py
+++ blog/models/category.py
@@ -5,6 +5,11 @@ from django.urls import reverse
 class Category(models.Model):
     name = models.CharField(max_length=100, help_text='Category name')
 
+    class Meta:
+        verbose_name = 'Category'
+        verbose_name_plural = 'Categories'
+        ordering = ['name']
+
     def __str__(self):
         return self.name
 
